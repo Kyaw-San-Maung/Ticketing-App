bd800296e8da8cb213f79338b0bba571cf7a781d
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-01-23 02:35:57 +0630

    Adding status bg color and data to be dynamic.

--- app/(components)/StatusDisplay.jsx
+++ app/(components)/StatusDisplay.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 
 const StatusDisplay = ({ status }) => {
+  const getColor = (status) => {
+    let color = "bg-gray-500";
+    switch (status.toLowerCase()) {
+      case (status = "done"):
+        color = "bg-green-500";
+        return color;
+      case (status = "started"):
+        color = "bg-yellow-500";
+        return color;
+      case (status = "not started"):
+        color = "bg-red-500";
+        return color;
+    }
+    return color;
+  };
   return (
-    <span className="inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-300">
+    <span
+      className={`inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 ${getColor(
+        status
+      )}`}
+    >
       {status}
     </span>
   );
